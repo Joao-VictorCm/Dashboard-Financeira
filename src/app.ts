936b59f8f7ae9b248a262dce801f77bf37787e2f
Joao-VictorCm <joaovictor27cm@gmail.com>
2025-05-08 21:01:42 -0300

    adD FUNÇÃO DE CRIAR E LER TOTOAS AS FUNÇÕES

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,50 @@
 import { transacoes } from ".";
+import { Transacao } from ".";
 
 function menu() {
   const menu = require("prompt-sync")({ sigint: true });
   const addDados = require("prompt-sync")({ sigint: true });
+
+  function coletarDados() {
+    let id = transacoes.length;
+    let descricao;
+    descricao = addDados(` Descrição `);
+    let valor;
+    valor = addDados(` Valor R$ `);
+    let tipo;
+    tipo = addDados(` entrada | saida `);
+    let data;
+    data = addDados(` Data `);
+    let categoria;
+    categoria = addDados(` Moradia | Alimentação | Transporte `);
+    const valorNumber = Number(valor);
+
+    return {
+      id,
+      descricao,
+      valor: valorNumber,
+      tipo,
+      data,
+      categoria,
+    };
+  }
+
+  function listarTransacao(lista: Transacao[]) {
+    return lista.map((index) => {
+      return console.log(
+        `ID:${index.id} | Categoria:${
+          index.categoria
+        } | Valor:${index.valor.toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        })} | Tipo:${index.tipo} | Data:${index.data} | Descrição: ${
+          index.descricao
+        }`
+      );
+    });
+  }
+
+  function deleteTransacao(lista: Transacao[], id: number) {}
 
   let opcao;
   opcao = menu(`
@@ -18,32 +60,15 @@ function menu() {
     const result = opcao;
     switch (result) {
       case "1":
-        let id = transacoes.length;
-        let descricao;
-        descricao = addDados(` Descrição `);
-        let valor;
-        valor = addDados(` Valor R$ `);
-        let tipo;
-        tipo = addDados(` entrada | saida `);
-        let data;
-        data = addDados(` Data `);
-        let categoria;
-        categoria = addDados(` Moradia | Alimentação | Transporte `);
-
-        const valorNumber = Number(valor);
-        const newItem = {
-          id,
-          descricao,
-          valor: valorNumber,
-          tipo,
-          data,
-          categoria,
-        };
-
-        transacoes.push(newItem);
+        const novaTransacao = coletarDados();
+        transacoes.push(novaTransacao);
         break;
-
-      default:
+      case "2":
+        break;
+      case "3":
+        break;
+      case "4":
+        listarTransacao(transacoes);
         break;
     }
     opcao = menu(`
