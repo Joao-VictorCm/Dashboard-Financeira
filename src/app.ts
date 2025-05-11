0c2f0b9c7674da8285418ab2b42c6b56e54206a0
Joao-VictorCm <joaovictor27cm@gmail.com>
2025-05-10 23:15:30 -0300

    Finalizado a api com crud

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,9 @@ import { Transacao } from ".";
 function menu() {
   const menu = require("prompt-sync")({ sigint: true });
   const addDados = require("prompt-sync")({ sigint: true });
+  const pesquisarID = require("prompt-sync")({ sigint: true });
+  const idParaAtualizar = require("prompt-sync")({ sigint: true });
+  const prompt = require("prompt-sync")({ sigint: true });
 
   function coletarDados() {
     let id = transacoes.length + 1;
@@ -57,13 +60,62 @@ function menu() {
     }
   }
 
+  function transacaoID(id: number) {
+    const transacao = transacoes.find((item) => item.id === id);
+    if (!transacao) {
+      console.log("Erro ao pesuqisar esta transação");
+    } else {
+      console.log(
+        `ID: ${transacao.id} | Categoria: ${
+          transacao.categoria
+        } | Valor: ${transacao.valor.toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        })} | Tipo: ${transacao.tipo} | Data: ${transacao.data} | Descrição: ${
+          transacao.descricao
+        }`
+      );
+    }
+  }
+
+  function atualizarTransacao(id: number) {
+    const transacao = transacoes.find((index) => index.id === id);
+    if (!transacao) {
+      console.log(`Essa transação não existe`);
+      return;
+    }
+    console.log("Atulizando a transação");
+    const novaDescricao = prompt(
+      `Nova descrição (atual: ${transacao.descricao}): `
+    );
+
+    const novoValor = prompt(`Novo valor (atual: R$ ${transacao.valor}): `);
+    const novoTipo = prompt(`Novo tipo (atual: ${transacao.tipo}): `);
+    const novaData = prompt(`Nova data (atual: ${transacao.data}): `);
+    const novaCategoria = prompt(
+      `Nova categoria (atual: ${transacao.categoria}): `
+    );
+
+    //Atualizando dados
+
+    if (novoTipo === "entrada" || novoTipo === "saida") {
+      transacao.tipo = novoTipo;
+    }
+
+    transacao.categoria = novaCategoria || transacao.categoria;
+    transacao.valor = novoValor
+      ? Number(novoValor)
+      : transacao.valor || transacao.valor;
+    transacao.data = novaData || transacao.data;
+    transacao.descricao = novaDescricao || transacao.descricao;
+  }
   let opcao;
   opcao = menu(`
     1 - Adicionar transação
     2 - Remover transação
-    3 -ar transação
+    3 - Listar por ID
     4 - Listar transações
-    5 - Ver relatórios
+    5 - Atuallizar transação
     6 - Sair
     `);
 
@@ -79,20 +131,28 @@ function menu() {
         id = addDados(` ID da transação a ser deleteda `);
         const idNumber = Number(id);
         deleteTransacao(idNumber);
-
         break;
       case "3":
+        let itemId;
+        itemId = pesquisarID(` ID da transação `);
+        const idPesquisado = Number(itemId);
+        transacaoID(idPesquisado);
         break;
       case "4":
         listarTransacao(transacoes);
+        break;
+      case "5":
+        let atualizacao;
+        atualizacao = idParaAtualizar(` ID para da trasação a ser atualizada `);
+        const idAtualizado = Number(atualizacao);
+        atualizarTransacao(idAtualizado);
         break;
     }
     opcao = menu(`
         1 - Adicionar transação
         2 - Remover transação
-        3 -ar transação
-        4 - Listar transações
-        5 - Ver relatórios
+        3 - Listar por ID
+        5 - Atuallizar transação
         6 - Sair
         `);
   }
